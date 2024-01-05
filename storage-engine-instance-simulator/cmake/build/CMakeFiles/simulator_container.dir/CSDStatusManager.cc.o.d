6187080d6fc676b9742d6b6555cd822e92d3a554
DrakwonJ <35063161+DrakwonJ@users.noreply.github.com>
2024-01-05 14:19:02 +0900

    Delete storage-engine-instance-simulator directory

--- storage-engine-instance-simulator/cmake/build/CMakeFiles/simulator_container.dir/CSDStatusManager.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/simulator_container.dir/CSDStatusManager.cc.o: \
- /root/KETI-Simulator/storage-engine-instance-simulator/CSDStatusManager.cc \
- /usr/include/stdc-predef.h
--- storage-engine-instance-simulator/cmake/build/CMakeFiles/simulator_container.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for simulator_container.
-# This may be replaced when dependencies are built.
