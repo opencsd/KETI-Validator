df11644f3747fad636f3d6ae418f80973a99d202
juhyunkil <wngus0331@gmail.com>
2025-05-21 15:18:19 +0900

    2025.05.21

--- StorageValidator.hpp
+++ StorageValidator.hpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include "DBManager.hpp"
 #include "keti_log.h"
+#include <thread>
+#include <chrono>
 std::string StorageValidatorMain(validationLog csdLog, std::vector<querySnippetInfo> snippetInfo, int queryNum, optionInfo option, int optionID, std::string userID, int simulationNnm, std::string returnJson);
 std::string getTimestampp();
  std::string convertTPCH2(std::string tpch);
